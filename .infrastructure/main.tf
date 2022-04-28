6d43c2fc0deeddee3c0370e5a2260e4289a9cd66
Thiago <thiduzz14@gmail.com>
2022-04-28 08:52:25 +0200

    Add variable files

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = "1.1.9"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.11.0"
     }
     scaleway = {
-      source = "scaleway/scaleway"
+      source  = "scaleway/scaleway"
       version = "2.2.1-rc.3"
     }
   }
@@ -14,13 +14,13 @@ terraform {
 
 provider "scaleway" {
   profile = "vinyl-catalog"
-  zone   = "fr-par-1"
-  region = "fr-par"
+  zone    = "fr-par-1"
+  region  = "fr-par"
 }
 
 provider "aws" {
   profile = "vinyl-catalog"
-  region = "eu-central-1"
+  region  = "eu-central-1"
 }
 
 module "aws_resources" {
@@ -29,4 +29,5 @@ module "aws_resources" {
 
 module "scaleway_resources" {
   source = "./scaleway"
+
 }
