86c5c335b1cf7e8d63a9a700497cb2bcff06e831
Thiago <thiduzz14@gmail.com>
2022-04-29 08:04:00 +0200

    Add basic setup for Kubernetes Cluster and node pool

---
+++ .infrastructure/scaleway/k8s.tf
@@ -0,0 +1,20 @@
+resource "scaleway_k8s_cluster" "main-cluster" {
+  name    = "${var.project_name}-cluster"
+  version = "1.23.0"
+  cni     = "cilium"
+  tags = [var.tag_environment]
+}
+
+resource "scaleway_k8s_pool" "main-pool" {
+  # Link this pool to the cluster
+  cluster_id = scaleway_k8s_cluster.main-cluster.id
+  name       = "${var.project_name}-pool"
+  # Define the node type we are going to be running
+  node_type  = "DEV1-M"
+  # The amount of nodes to run
+  size       = 1
+  autohealing = true
+  autoscaling = false
+  wait_for_pool_ready = true
+  tags = [var.tag_environment]
+}
