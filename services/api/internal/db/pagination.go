f4c9868c857768b6248d428e7d1f6b9d2d354117
Thiago <thiduzz14@gmail.com>
2022-05-20 09:31:55 +0200

    Scaffold collection repository and add pagination scope

---
+++ services/api/internal/db/pagination.go
@@ -0,0 +1,12 @@
+package db
+
+import (
+	"gorm.io/gorm"
+)
+
+func Paginate(page int, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
