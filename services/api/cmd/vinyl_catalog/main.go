842e5c5c61dc456adda53f77ff3e3dc3edc38b94
Thiago <thiduzz14@gmail.com>
2022-05-20 09:35:58 +0200

    Create App that will be responsible for mapping handlers and injecting dependencies

--- services/api/cmd/vinyl_catalog/main.go
+++ services/api/cmd/vinyl_catalog/main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	driver "github.com/go-sql-driver/mysql"
+	"github.com/thiduzz/vinyl-catalog/cmd/vinyl_catalog/app"
 	"log"
 	"net/http"
 )
 
 func main() {
-	log.Println("Starting server at port 8080")
-
-	http.HandleFunc("/up", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "God save the Queen!")
-	})
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Setting Database connection...")
+	databaseConnection, err := app.NewMySqlDatabaseConnection(databaseConfig())
+	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Setting Application and Dependencies...")
+	application := app.NewApp(databaseConnection)
+		log.Fatal(err)
+	}
+}
+
+func databaseConfig() *driver.Config {
+	config := driver.NewConfig()
+	config.User = os.Getenv("DB_USER_NAME")
+	config.Passwd = os.Getenv("DB_USER_PASSWORD")
+	config.Net = "tcp"
+	config.Addr = fmt.Sprintf("%s:%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
+	config.DBName = os.Getenv("DB_SCHEMA_NAME")
+	config.Params = map[string]string{"charset": "utf8"}
+	config.Loc = time.UTC
+	config.ParseTime = true
+	return config
 }
